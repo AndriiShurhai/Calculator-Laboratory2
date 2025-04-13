466cca87d9139c7570f8cfdda8d3b46fd90c256e
Andrii Shurhai <shurgaiandrew335@gmail.com>
2025-04-13 19:19:54 +0300

    Adding CalculationCommand class implementing interface ICommand (for command pattern), adding Calculator class to store the state of the calculator.

---
+++ WpfApp1/WpfApp1/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using System.Windows.Media;
+
+namespace CalculatorProject
+{
+    class Calculator
+    {
+        public double PreviousValue { get; set; }
+        public double CurrentValue { get; set; }
+        public string PendingOperation { get; set; }
+        public string DisplayText { get; set; }
+        public bool IsNewInput { get; set; }
+
+        public Calculator()
+        {
+            Clear();
+        }
+
+        public void Clear()
+        {
+            CurrentValue = 0;
+            PreviousValue = 0;
+            PendingOperation = "";
+            DisplayText = "0";
+            IsNewInput = true;
+        }
+    }
+}
--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Runtime.CompilerServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,16 +10,35 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Calculator
+namespace CalculatorProject
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Calculator _calculator;
+        private Stack<ICommand> _undoStack;
+        private Stack<ICommand> _redoStack;
+
         public MainWindow()
         {
             InitializeComponent();
+            _calculator = new Calculator();
+            _undoStack = new Stack<ICommand>();
+            _redoStack = new Stack<ICommand>();
+
+            UpdateDisplay();
+
+            KeyDown += MainWindow_KeyDown;
+        }
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("key pressed");
+        }
+        private void UpdateDisplay()
+        {
+            txtDisplay.Text = _calculator.DisplayText;
         }
     }
 }
